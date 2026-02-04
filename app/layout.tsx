312205ffc0804d7a30a549bc952daa74fa836a65
vivek <dvivek.mails@gmail.com>
2026-02-04 23:14:03 +0800

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`} suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) - AW-17930856536 */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17930856536"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17930856536');
+            `,
+          }}
+        />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
